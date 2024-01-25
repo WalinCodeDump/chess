ac339e57bbc2113c44571a28d606239dd4cd3204
Ian Jackson <jjackson.iian@gmail.com>
2024-01-25 16:42:24 -0700

    Playing around with Java

---
+++ playing field/src/GuyMan.java
@@ -0,0 +1,8 @@
+public class GuyMan {
+    public static void says(String catchphrase) {
+        System.out.println(catchphrase);
+    }
+    public static String phrase(String inputphrase) {
+        return inputphrase;
+    }
+}
--- playing field/src/Main.java
+++ playing field/src/Main.java
@@ -1,13 +1,22 @@
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+/*        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println(i + ". Software Construction");
         }
+
+ */
+
+        System.out.println("Ok now let's do this:\n\n");
+        GuyMan sayer = new GuyMan();
+        sayer.says("I'm here to program, innit");
     }
 }
+
