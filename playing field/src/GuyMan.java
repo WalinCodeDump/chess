df1fb0f3f85a952d8380b0c929835325f55d5d6a
Ian Jackson <jjackson.iian@gmail.com>
2024-03-06 17:08:09 -0700

    Moved extraneous non-chess folders; now locally stored only

--- playing field/src/GuyMan.java
+++
@@ -1,8 +0,0 @@
-public class GuyMan {
-    public static void says(String catchphrase) {
-        System.out.println(catchphrase);
-    }
-    public static String phrase(String inputphrase) {
-        return inputphrase;
-    }
-}
