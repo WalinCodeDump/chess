cfdcb3fb6274a5c753b5ab865b2373b229ef34ce
Walin CD <jjackson.iian@gmail.com>
2026-01-20 01:09:55 -0700

    Chess Phase 0 video tutorial part 2

---
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -0,0 +1,11 @@
+package chess;
+
+import java.util.Collection;
+
+public class BishopMovesCalculator implements PieceMovesCalculator {
+
+    Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        throw new RuntimeException("Not implemented");
+    }
+
+}
