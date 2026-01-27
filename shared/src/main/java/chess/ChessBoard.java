a42a4616d59a8ac2d3c7ba4ff7bbd694266e9567
Walin CD <jjackson.iian@gmail.com>
2026-01-27 14:26:42 -0700

    Chess Phase 0 - passed Reset Board test

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -46,23 +49,55 @@ public class ChessBoard {
             // White row of pawns
             squares[0+1][n-1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         }
+
+        // Hm...for rooks, knights, and bishops, it's two along one row and two along another row.
+        // Could I do something with modulo arithmetic to do that? Would it be any better than
+        // just four lines? Probably not. Would need to add a line for the TeamColor, etc.
+
         // Rooks
-        // Not implemented
+        // Black pair of rooks
+        squares[8-1][1-1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        squares[8-1][8-1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        // White pair of rooks
+        squares[1-1][1-1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        squares[1-1][8-1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
 
         // Bishops
-        // Not implemented
+        // Black pair of bishops
+        squares[8-1][3-1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+        squares[8-1][6-1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+        // White pair of bishops
+        squares[1-1][3-1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        squares[1-1][6-1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
 
         // Knights
-        // Not implmented
+        // Black pair of knights
+        squares[8-1][2-1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+        squares[8-1][7-1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+        // White pair of knights
+        squares[1-1][2-1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        squares[1-1][7-1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
 
         // Kings/Queens
-        // Not implemented
-
-        throw new RuntimeException("Not implemented");
+        // Black king/queen
+        squares[8-1][5-1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+        squares[8-1][4-1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+        // White king/queen
+        squares[1-1][5-1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+        squares[1-1][4-1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
 
-
-
-
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
+    }
 }
