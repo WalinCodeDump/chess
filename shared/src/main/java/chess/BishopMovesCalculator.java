7ab1852511b941e0abaffe65b47b79f37f933d27
Walin CD <jjackson.iian@gmail.com>
2026-01-26 23:32:58 -0700

    Chess Phase 0 - added positionIsValid

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 public class BishopMovesCalculator implements PieceMovesCalculator {
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        throw new RuntimeException("Not implemented");
         // Start writing...
         // 1 - Move around in all four directions.
         // 2 - Do checks
@@ -16,12 +15,28 @@ public class BishopMovesCalculator implements PieceMovesCalculator {
         // So... - Iterate moving outwards first. Stop iterating after any of the above.
         // Actually, also stop iterating a direction after encountering opposite color.
 
-        ChessPosition currpos = new ChessPosition(position.getRow(), position.getColumn());
+        int curr_row = position.getRow();
+        int curr_col = position.getColumn();
+
         // Northwest
         for (int i = 1; i <= 8; i++) {
             // Check for collisions! With edge, with piece
-            var a = 1; // Stubbed
-        }
-    }
+            // Collision with edge:
+            if (curr_row + i > 8 || curr_col + i > 8)
+                break;
 
+            // Create new position (will remove later if it collides)
+            ChessPosition addpos = new ChessPosition(curr_row + i, curr_col + i);
+
+            // Recreate the piece for clarity
+            // Does not check if it's the right type...assuming that it's already
+            // been checked.
+            ChessGame.TeamColor curr_color = board.getPiece(position).getTeamColor();
+            ChessPiece curr_piece = new ChessPiece(curr_color, ChessPiece.PieceType.BISHOP);
+            positionIsValid(board,position,curr_piece);
+        }
+        // Northeast
+        // Southeast
+        // Southwest
+    }
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -4,4 +4,22 @@ import java.util.Collection;
 
 public interface PieceMovesCalculator {
     Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
-}
+    default boolean positionIsValid(ChessBoard board, ChessPosition position, ChessPiece piece) {
+        boolean isValid = true;
+        // Within rows of chess board
+        if (position.getRow() < 1 || position.getRow() > 8)
+            isValid = false;
+
+        // Within columns of chess board
+        if (position.getColumn() < 1 || position.getColumn() > 8)
+            isValid = false;
+        // Create new position (will remove later if it collides)
+        ChessPosition addpos = new ChessPosition(position.getRow(), position.getColumn());
+
+        // Check if the piece is moving onto another piece
+        if (board.getPiece(addpos) != null) {
+            // Check if it's the same color or not
+            if (board.getPiece(addpos).getTeamColor() == piece.getTeamColor())
+                isValid = false;
+        }
+    }}
